22d446a07015e949acd46403a8c4bb6783f80fae
Angel Ramallo <alu0100312898@ull.edu.es>
2021-11-16 19:13:44 +0000

    entrando en despliegue

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Mi primer post",
+        "excerpt":"Les presento mi carpeta de trabajos   Que, como verán, han sido pocos, pero entrañables, con mucha dedicación y aprendizaje   \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] ","categories": ["curiosidades"],
+        "tags": [],
+        "url": "/curiosidades/aet",
+        "teaser": null
+      },{
+        "title": "Mi último proyecto",
+        "excerpt":"Granada (Spain) Proyecto de adecuación de local Buenavida en Granada (Spain) Próximamente les mostraré mi último trabajo en Granada, que también ha sido el primero como director técnico de obra: la renovación del Centro de Salud y Deporte Buenavida. Con un presupuesto de ejecución aproximado de 15 mil euros Terminado...","categories": [],
+        "tags": [],
+        "url": "/aet",
+        "teaser": null
+      },{
+        "title": "Clase",
+        "excerpt":"Aprendiendo a usar un generador estático   Me he cargado mi segundo post  ","categories": ["curiosidades"],
+        "tags": [],
+        "url": "/curiosidades/clase",
+        "teaser": null
+      }]
